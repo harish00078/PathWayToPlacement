be99f9dcaa83f19b7efe2690b89d4f07f717c15e
harish kumar <harishpathania150@gmail.com>
2024-07-09 17:34:20 +0530

    crypto tracker app

--- Project-Assignments/Crypto_Tracker/js/app.js
+++ Project-Assignments/Crypto_Tracker/js/app.js
@@ -1,23 +1,51 @@
-const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    "x-cg-demo-api-key": "CG-bTUWVZGY95RCTnRdid3bdM79",
+  },
+};
+let coins = [];
 
- const options = {
-   method: "GET",
-   headers: {
-     accept: "application/json",
-     "x-cg-demo-api-key": "CG-bTUWVZGY95RCTnRdid3bdM79",
-   },
- };
-const coins = [];
+// fetching the data from api:
+const fetchCoins = async () => {
+  try {
+    const response = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+      options
+    );
+    console.log(response);
+    const coinsData = await response.json();
+    console.log("coinsData:", coinsData);
+    return coinsData;
+  } catch (error) {
+    console.error("Error while fetching coins", error);
+  }
+};
+const displayCoins = (coins) => {
+  const tableBody = document.getElementById("crypto-table-body");
+  tableBody.innerHTML = "";
+  coins.forEach((coin, index) => {
+    const row = document.createElement("tr");
 
- const fetchCoins = async () => {
-    try {
-      const response = await fetch(url, options);
-      console.log(response);
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error('Error while fetching coins',error);
-    }
- }
- window.onload = fetchCoins();
+    row.innerHTML = `           
+                <td>${index + 1}</td>
+                <td><img width="24" height="24" src=${coin.image}alt=${
+      coin.name
+    }></td>
+                <td>${coin.name}</td>
+                <td>${coin.current_price}</td>
+                <td>${coin.total_volume}</td>
+                <td>${coin.market_cap}</td>
+                <td><i class="fa-regular fa-star"></i></td>
+              `;
+    tableBody.appendChild(row);
+  });
+};
 
+// showing the data:
+document.addEventListener("DOMContentLoaded", async () => {
+  coins = await fetchCoins();
+  console.log("coins:", coins);
+  displayCoins(coins);
+});
